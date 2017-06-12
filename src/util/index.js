47d9ca5d6f4c1955445ff37360d0e801d01c98cb
zanjs <root@zanjs.com>
2017-06-12 14:40:37 +0800

    Proper preparation solves 80 percent of life's problems :kissing_closed_eyes:

--- src/util/index.js
+++ src/util/index.js
@@ -9,6 +9,13 @@ export const isChinese = temp => {
   return false
 }
 
+export const isLangJP = temp => {
+  if (/.*[\u0800-\u4e00]+.*$/.test(temp.replace(/(^\s*)|[\r\n]|(\s*$)/g, ''))) {
+    return true
+  }
+  return false
+}
+
 export const isAllChinese = temp => {
   return /^[\u4E00-\u9FA5]+$/.test(temp.replace(/(^\s*)|(\s*$)/g, ''))
 }
